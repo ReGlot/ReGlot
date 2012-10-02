523af2c7a9f06741126c7ef2ce526bf78687a523
Christian Foellmann <foellmann@foe-services.de>
2012-10-02 14:24:18 +0200

    template extension - HTML5 tags + a few more DOM elements for future themability

--- gp-templates/footer.php
+++ gp-templates/footer.php
@@ -1,6 +1,11 @@
-	<p id="footer" class="secondary">
-		<?php printf(__('<a href="https://github.com/fmestrone/ReGlot" title="Found in translation, and some..."><b>%s</b></a>, powered by <a href="http://glotpress.org/" title="Found in translation">GlotPress</a>, improved by <a href="http://www.federicomestrone.com" title="Federico Mestrone">FedMest</a>.'), gp_app_name()); ?>
-		<?php gp_footer(); ?>
-	</p>
-	</body>
+<footer class="secondary">
+    <div class="align-left">
+        <?php printf(__('<a href="https://github.com/fmestrone/ReGlot" title="Found in translation, and some..." target="_blank"><b>%s</b></a>, powered by <a href="http://glotpress.org/" title="Found in translation" target="_blank">GlotPress</a>, improved by <a href="http://www.federicomestrone.com" title="Federico Mestrone" target="_blank">FedMest</a>.'), gp_app_name()); ?>
+    </div>
+    <div class="align-right">
+        &nbsp;
+    </div>
+</footer>
+<?php gp_footer(); ?>
+</body>
 </html>
