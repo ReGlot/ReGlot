12ae4957438c3fb8dbeaace5b796c37346031436
Christian Foellmann <foellmann@foe-services.de>
2012-10-02 14:24:18 +0200

    template extension - HTML5 tags + a few more DOM elements for future themability

--- gp-templates/footer.php
+++
@@ -1,6 +0,0 @@
-	<p id="footer" class="secondary">
-		<?php printf(__('Powered by <a href="http://www.reglot.org" title="Social Translating"><b>Re&sdot;Glot</b></a>'), gp_app_name()); ?>
-		<?php gp_footer(); ?>
-	</p>
-	</body>
-</html>
