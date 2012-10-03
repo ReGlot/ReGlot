25cf83bb55716d62c1fd9301c4d289ae843de6ef
Christian Foellmann <foellmann@foe-services.de>
2012-10-03 20:34:00 +0200

    footer.php merge

---
+++ gp-templates/footer.php
@@ -0,0 +1,11 @@
+<footer class="secondary">
+    <div class="align-left">
+        <?php printf(__('Powered by <a href="http://www.reglot.org" title="Social Translating"><b>Re&sdot;Glot</b></a>'), gp_app_name()); ?>
+    </div>
+    <div class="align-right">
+        &nbsp;
+    </div>
+</footer>
+<?php gp_footer(); ?>
+</body>
+</html>
