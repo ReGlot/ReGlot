0168334d42828786f771a72e53ef9da724d9f9d9
Christian Foellmann <foellmann@foe-services.de>
2012-12-07 23:58:59 +0100

    bootstrap style

--- gp-templates/bootstrap/footer.php
+++ gp-templates/bootstrap/footer.php
@@ -1,6 +1,8 @@
+<hr>
 <footer>
     <p><?php printf(__('Powered by <a href="http://www.reglot.org" title="Social Translating"><b>Re&sdot;Glot</b></a>'), gp_app_name()); ?></p>
 </footer>
+</div><!--/.fluid-container-->
 <?php gp_footer(); ?>
 </body>
 </html>
--- gp-templates/bootstrap/header.php
+++ gp-templates/bootstrap/header.php
@@ -10,19 +10,19 @@ wp_enqueue_script('common');
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
-        
+
         <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
             <script type="text/javascript" src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
-        
+
         <!-- favicon.ico and touch icons -->
-        <link rel="shortcut icon" href="wp-templates/bootstrap/ico/favicon.ico">
+        <link rel="shortcut icon" href="gp-templates/bootstrap/ico/favicon.ico">
         <link rel="apple-touch-icon-precomposed" sizes="144x144" href="gp-templates/bootstrap/ico/apple-touch-icon-144-precomposed.png">
         <link rel="apple-touch-icon-precomposed" sizes="114x114" href="gp-templates/bootstrap/ico/apple-touch-icon-114-precomposed.png">
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="gp-templates/bootstrap/ico/apple-touch-icon-72-precomposed.png">
         <link rel="apple-touch-icon-precomposed" href="gp-templates/bootstrap/ico/apple-touch-icon-57-precomposed.png">
-    
+
         <link rel="stylesheet" href="gp-templates/bootstrap/css/bootstrap.css" type="text/css" media="all" />
         <link rel="stylesheet" href="gp-templates/bootstrap/css/bootstrap-responsive.css" type="text/css" media="all" />
         <script type="text/javascript" src="gp-templates/bootstrap/js/bootstrap.js"></script>
@@ -32,6 +32,46 @@ wp_enqueue_script('common');
         <script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
         <div id="gp-js-message"></div>
         <header>
+            <div class="navbar navbar-inverse navbar-fixed-top">
+                <div class="navbar-inner">
+                    <div class="container-fluid">
+                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </a>
+                        <a class="brand" href="<?php echo gp_url('/'); ?>">ReGlot</a>
+                        <div class="nav-collapse collapse">
+                            <p class="navbar-text pull-right">
+                                <?php
+                                if (GP::$user && GP::$user->logged_in()) {
+                                ?>
+                                <a href="<?php echo gp_url_users() ?>" class="navbar-link"><?php _e('Users'); ?></a>
+                                <a href="<?php echo gp_url_user_profile() ?>" class="navbar-link"><?php _e('Profile'); ?></a>
+                                <a href="<?php echo gp_url_logout() ?>" class="navbar-link"><?php _e('Log out'); ?>
+                                    <?php
+                                } else if (GP::$user) {
+                                    ?>
+                                <?php if (gp_get_option('user_registration') == 'on') { ?>
+                                <strong><a href="<?php echo gp_url_register(); ?>" class="navbar-link"><?php _e('Register'); ?></a></strong>
+                                <?php } ?>
+                                <strong><a href="<?php echo gp_url_login(); ?>" class="navbar-link"><?php _e('Log in'); ?></a></strong>
+                                <?php } ?>
+                            </p>
+                            <ul class="nav">
+                                <li class="active"><a href="#">Home</a></li>
+                                <li><a href="#about">About</a></li>
+                                <li><a href="#contact">Contact</a></li>
+                            </ul>
+                        </div><!--/.nav-collapse -->
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
             <h1>
                 <a class="logo" href="<?php echo gp_url('/'); ?>">
                     <img alt="<?php esc_attr(__('GlotPress logo')); ?>" src="<?php echo gp_url_img('glotpress-logo.png'); ?>" />
@@ -42,8 +82,9 @@ wp_enqueue_script('common');
                         <?php
                         if (GP::$user && GP::$user->logged_in()) {
                             ?>
-                            <li class="first"><?php $user = GP::$user->current();
-                            printf(__('Welcome %s! &nbsp; &nbsp; '), $user->display_name);
+                            <li class="first"><?php
+                        $user = GP::$user->current();
+                        printf(__('Welcome %s! &nbsp; &nbsp; '), $user->display_name);
                             ?></li>
                             <?php
                         }
@@ -67,11 +108,11 @@ wp_enqueue_script('common');
                             <li><a href="<?php echo gp_url_users() ?>"><?php _e('Users'); ?></a></li>
                             <li><a href="<?php echo gp_url_user_profile() ?>"><?php _e('Profile'); ?></a></li>
                             <li><a href="<?php echo gp_url_logout() ?>"><?php _e('Log out'); ?></a>
-                            <?php
-                        } else if (GP::$user) {
-                            ?>
+                                <?php
+                            } else if (GP::$user) {
+                                ?>
                             <?php if (gp_get_option('user_registration') == 'on') { ?>
-                            <li><strong><a href="<?php echo gp_url_register(); ?>"><?php _e('Register'); ?></a></strong></li>
+                                <li><strong><a href="<?php echo gp_url_register(); ?>"><?php _e('Register'); ?></a></strong></li>
                             <?php } ?>
                             <li><strong><a href="<?php echo gp_url_login(); ?>"><?php _e('Log in'); ?></a></strong></li>
                             <?php
@@ -82,14 +123,14 @@ wp_enqueue_script('common');
                 <div class="clearfix"></div>
             </h1>
         </header>
-        <?php if (gp_notice('error')): ?>
+            <?php if (gp_notice('error')): ?>
             <div class="error">
-                <?php echo gp_notice('error'); //TODO: run kses on notices ?>
+            <?php echo gp_notice('error'); //TODO: run kses on notices  ?>
             </div>
         <?php endif; ?>
-        <?php if (gp_notice()): ?>
+            <?php if (gp_notice()): ?>
             <div class="notice">
-                <?php echo gp_notice(); ?>
+            <?php echo gp_notice(); ?>
             </div>
         <?php endif; ?>
-        <?php do_action('after_notices'); ?>
+<?php do_action('after_notices'); ?>
